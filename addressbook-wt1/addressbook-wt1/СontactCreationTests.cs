28dd6fd1a4e6ab111b496d70df3f769c7a2bd978
Vasek897 <98513972+Vasek897@users.noreply.github.com>
2023-12-17 12:26:13 +0600

    Update СontactCreationTests.cs
    Переименовал тест

--- addressbook-wt1/addressbook-wt1/СontactCreationTests.cs
+++ addressbook-wt1/addressbook-wt1/СontactCreationTests.cs
@@ -41,7 +41,7 @@ namespace WebAddressbookTests
         }
 
         [Test]
-        public void TheUntitledTestCaseTest()
+        public void ContactCreationTest()
         {
             OpenHomePage();
             Login(new AccountData("admin", "secret")); 
